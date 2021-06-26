e628d1839c53ce757bc9960a944824c6dfe63c93
Jayan Varsani <jayan.varsani@student.manchester.ac.uk>
2021-06-26 12:22:53 +0100

    Added Redux By Configurating userReducer/userActions & Implementing Via connect()/mapStateToProps/mapDispatchToProps In Header/App

--- src/App.js
+++ src/App.js
@@ -7,34 +7,28 @@ import Header from "./components/header/header.component";
 import SignInRegisterPage from "./pages/sign-in-register/sign-in-register.component";
 import { auth } from "./firebase/firebase.utils";
 import { createUserProfileDocument } from "./firebase/firebase.utils";
+import { connect } from "react-redux";
 
 import "./App.css";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
-	constructor() {
-		super();
-
-		this.state = {
-			currentUser: null,
-		};
-	}
-
 	unsubscribeFromAuth = null;
 	componentDidMount() {
+		console.log(connect(null, mapDispatchToProps));
+		console.log(connect(null, mapDispatchToProps)(App));
+		const { setCurrentUser } = this.props;
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
 			if (userAuth) {
 				const userRef = await createUserProfileDocument(userAuth);
 				userRef.onSnapshot((snapShot) => {
-					this.setState({
-						currentUser: {
-							id: snapShot.id,
-							...snapShot.data(),
-						},
+					setCurrentUser({
+						id: snapShot.id,
+						...snapShot.data(),
 					});
 				});
-			} else {
-				this.setState({ currentUser: userAuth });
 			}
+			setCurrentUser(userAuth);
 		});
 	}
 
@@ -45,7 +39,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Header currentUser={this.state.currentUser} />
+				<Header />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
@@ -56,4 +50,26 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+// 'mapDispatchToProps' returns object containing the props
+// props contains setCurrentUser function which dispatches the action object
+// action object dispatched/fired to all reducers in order to update master state
+// 'dispatch' parameter provides the function which is invoked dispatch() later, to act as setState()
+const mapDispatchToProps = (dispatch) => ({
+	setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+// null represents function 'mapStateToProps' as connect looks for parameters 'mapStateToProps' & 'mapDispatchToProps'
+// null because we don't need a function to map state to props because no props are used in App
+// App is top level & isn't passed anything so we dont need a function to convert state to feed as props to App
+
+// 'mapDispatchToProps' function forwards all the dispatch props to the component
+// so we use our custom setCurrentUser function in our component via a prop which does the dispatch within
+export default connect(null, mapDispatchToProps)(App);
+
+// mapDispatchToProps:
+// by default, components receive 'dispatch' function as a prop by default
+// used when we want to explicitly dispatch in the component by directly invoking dispatch() where needed
+// mapDispatchToProps allows us to create functions, that dispatch for us, & provide them to the component
+// so we do not need to directly call dispatch() but through the alias of a function accessed via a prop
+// if you provide a 'mapDispatchToProps', connected component no longer recieves 'dispatch' as a prop
+// it knows to instead pass 'mapDispatchToProps' as this will contain our custom functions perform the dispatch
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,11 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import  {auth} from '../../firebase/firebase.utils'
 import { ReactComponent as Logo } from '../../assets/logo/crown.svg';
-
+import {connect} from 'react-redux'
 import './header.styles.scss';
 
+// if currentUser state is changed, re-render
 const Header = ({currentUser}) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
@@ -27,4 +27,12 @@ const Header = ({currentUser}) => (
   </div>
 );
 
-export default Header;
+// catches the updated state and pulls out current user and pass it into Header
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
+
+// connect returns a function which will connect the Header to mapStateToProps
+// allows the state to be destructured as props and then connect that to Header
+// allowing store to act as if we used default state management with state and props 
+export default connect(mapStateToProps)(Header);
