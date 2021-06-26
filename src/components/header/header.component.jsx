4094bfee1e37225a48375b20647847600101dd73
Jayan Varsani <jayan.varsani@student.manchester.ac.uk>
2021-06-26 23:19:07 +0100

    Added Cart Icon Component & Cart Dropdown Component & Implemented Via Redux To Show/Hide

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import  {auth} from '../../firebase/firebase.utils'
-import { ReactComponent as Logo } from '../../assets/logo/crown.svg';
-import {connect} from 'react-redux'
+import { ReactComponent as Logo } from '../../assets/icons/crown.svg';
+import { connect } from 'react-redux'
 import './header.styles.scss';
-
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart/cart-dropdown.component';
 // if currentUser state is changed, re-render
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -22,14 +23,19 @@ const Header = ({currentUser}) => (
           <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
           :
           <Link className="option" to='/signin'>SIGN IN</Link>
-        }
+      }
+      <Link className="option" to='/cart'>
+        <CartIcon/>
+      </Link>
     </div>
+    { hidden ? null : <CartDropdown />}
   </div>
 );
 
 // catches the updated state and pulls out current user and pass it into Header
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+  currentUser,
+  hidden
 })
 
 // connect returns a function which will connect the Header to mapStateToProps
