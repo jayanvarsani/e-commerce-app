018d4da12d7987092ac25316fdf976a93d6fdbc9
Jayan Varsani <jayan.varsani@student.manchester.ac.uk>
2021-08-03 13:22:33 +0100

    Separating CRA App Into Client Folder - Added Express Server To Handle Backend Stripe Payments

--- src/components/cart/cart-dropdown.container.jsx
+++
@@ -1,6 +0,0 @@
-import CartDropdown from './cart-dropdown.component';
-import withRouter from '../menu-item/menu-item.component';
-
-const CartDropdownContainer = withRouter(CartDropdown)
-
-export default CartDropdownContainer
