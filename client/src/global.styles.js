ba48eb632c8a9e9f64ed41d19e4eea909e546526
Jayan Varsani <jayan.varsani@student.manchester.ac.uk>
2021-08-19 13:35:33 +0100

    PWA Compliancy, Service Worker, HTTPS & Manifest

--- client/src/global.styles.js
+++ client/src/global.styles.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap");
     body {
         font-family: "Noto Sans JP", sans-serif;
         padding: 20px 60px;
--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
+import * as serviceWorker from "./serviceWorkerRegistration";
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -25,3 +26,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorker.register();
